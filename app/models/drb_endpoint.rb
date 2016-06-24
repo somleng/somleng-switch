8db46a8dfcce245095c53218c0fda0665d2d8f68
David Wilkie <dwilkie@gmail.com>
2016-06-24 17:32:14 +0700

    Set .ruby-version and fix segfault with eventmachine

---
+++ app/models/drb_endpoint.rb
@@ -0,0 +1,7 @@
+class DrbEndpoint
+  def place_call(call_json)
+    p call_json
+    call = Adhearsion::OutboundCall.originate("SIP/#{number}@#{provider}", :controller => CallController)
+    call.id
+  end
+end
--- config/adhearsion.rb
+++ config/adhearsion.rb
@@ -2,4 +2,7 @@ Adhearsion.config do |config|
   config.development do |dev|
     dev.platform.logging.level = :debug
   end
+#  config.drb do |config|
+#    config.shared_object = DrbEndpoint
+#  end
 end
