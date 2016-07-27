e43d0eb33e0c86c3bd3cc039afafc9eb87ca486f
David Wilkie <dwilkie@gmail.com>
2016-07-27 17:37:41 +0700

    Rename drb method

--- app/models/drb_endpoint.rb
+++ app/models/drb_endpoint.rb
@@ -1,5 +1,5 @@
 class DrbEndpoint
-  def place_call(call_json)
+  def initiate_outbound_call!(call_json)
     p call_json
 #    call = Adhearsion::OutboundCall.originate("SIP/#{number}@#{provider}", :controller => CallController)
 #    call.id
