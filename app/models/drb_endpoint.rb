a3f6d27c9efc85deb1aa76631beda7e0cc07e45b
David Wilkie <dwilkie@gmail.com>
2016-07-27 17:09:28 +0700

    Configure adhearsion-drb

--- app/models/drb_endpoint.rb
+++ app/models/drb_endpoint.rb
@@ -1,7 +1,7 @@
 class DrbEndpoint
   def place_call(call_json)
     p call_json
-    call = Adhearsion::OutboundCall.originate("SIP/#{number}@#{provider}", :controller => CallController)
-    call.id
+#    call = Adhearsion::OutboundCall.originate("SIP/#{number}@#{provider}", :controller => CallController)
+#    call.id
   end
 end
--- config/adhearsion.rb
+++ config/adhearsion.rb
@@ -1,8 +1,11 @@
+require_relative '../app/models/drb_endpoint'
+
 Adhearsion.config do |config|
   config.development do |dev|
     dev.platform.logging.level = :debug
   end
-#  config.drb do |config|
-#    config.shared_object = DrbEndpoint
-#  end
+
+  config.adhearsion_drb do |drb|
+    drb.shared_object = DrbEndpoint.new
+  end
 end
