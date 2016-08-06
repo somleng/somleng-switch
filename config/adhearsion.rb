5eb040d2f121c7d776249f3e229e1d39a5810b7b
David Wilkie <dwilkie@gmail.com>
2016-08-06 11:04:34 +0700

    use ENV vars for configuration

--- config/adhearsion.rb
+++ config/adhearsion.rb
@@ -1,10 +1,6 @@
 require_relative '../app/models/drb_endpoint'
 
 Adhearsion.config do |config|
-  config.development do |dev|
-    dev.platform.logging.level = :debug
-  end
-
   config.adhearsion_drb do |drb|
     drb.shared_object = DrbEndpoint.new
   end
