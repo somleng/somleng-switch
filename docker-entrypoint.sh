14224fcad6c7312068f40fd02ae782d4ae0fd46c
David Wilkie <dwilkie@gmail.com>
2017-09-25 13:43:47 +0700

    Set AHN_ADHEARSION_DRB_HOST to the HOSTNAME of the container if not set. #21

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,8 @@
 set -e
 
 if [ "$1" = 'ahn' ]; then
+  export AHN_ADHEARSION_DRB_HOST="${AHN_ADHEARSION_DRB_HOST:-$HOSTNAME}"
+
   if [ -n "$ADHEARSION_CONFIG_S3_PATH" ]; then
     # Pull Adhearsion configuration from S3
     eval $(aws s3 cp ${ADHEARSION_CONFIG_S3_PATH} - | sed 's/^/export /')
