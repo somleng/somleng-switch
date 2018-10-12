6a194a6bae199566633dc61b9e75a4c2bca9f26b
David Wilkie <dwilkie@gmail.com>
2018-10-12 17:58:11 +0700

    Fix config

--- config/adhearsion.rb
+++ config/adhearsion.rb
@@ -1,14 +1,13 @@
 require_relative "../app/models/drb_endpoint"
 
 Adhearsion.config do |config|
+  # Core Settings
   config.core.http.enable = false
 
-  config.adhearsion_drb do |drb|
-    drb.shared_object = DrbEndpoint.new
-    drb.adhearsion_drb.acl.allow = "all"
-  end
+  # DRb Settings
+  config.adhearsion_drb.shared_object = DrbEndpoint.new
+  config.adhearsion_drb.acl.allow = "all"
 
-  config.twilio do |twilio|
-    twilio.rest_api_enabled = "1"
-  end
+  # Twilio Settings
+  config.twilio.rest_api_enabled = "1"
 end
