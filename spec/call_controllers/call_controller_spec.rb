06667a8d3071e527b586d873855ce6d41060c2ac
David Wilkie <dwilkie@gmail.com>
2019-11-02 12:01:26 +0700

    Switch to CircleCI (#52)

--- spec/call_controllers/call_controller_spec.rb
+++ spec/call_controllers/call_controller_spec.rb
@@ -4,7 +4,7 @@ describe CallController do
   describe "#run" do
     it "handles the call via adhearsion-twilio" do
       call = instance_double(Adhearsion::Call)
-      call_controller = described_class.new(call)
+      call_controller = CallController.new(call)
       allow(call_controller).to receive(:notify_voice_request_url)
 
       call_controller.run
