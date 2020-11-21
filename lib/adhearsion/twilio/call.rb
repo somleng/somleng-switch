d1a6eebbc1344096a865addff6668fb4e6b19ee4
Samnang Chhun <samnang.chhun@gmail.com>
2020-11-21 11:02:06 +0700

    WIP

--- lib/adhearsion/twilio/call.rb
+++ lib/adhearsion/twilio/call.rb
@@ -1,23 +1,24 @@
 require_relative "util/number_normalizer"
 
 class Adhearsion::Twilio::Call
-  attr_accessor :call, :to, :from
+  attr_reader :call
+
+  delegate :id, :variables, to: :call
 
   def initialize(call)
-    self.call = call
-    set_call_variables!
-  end
-
-  def id
-    call.id
+    @call = call
   end
 
   def duration
     call.duration.to_i
   end
 
-  def variables
-    call.variables
+  def from
+    @from ||= normalize_from
+  end
+
+  def to
+    @to ||= number_normalizer.normalize(call.to)
   end
 
   private
@@ -26,23 +27,13 @@ class Adhearsion::Twilio::Call
     @number_normalizer ||= Adhearsion::Twilio::Util::NumberNormalizer.new
   end
 
-  def set_call_variables!
-    normalize_from!
-    normalize_to!
-  end
+  def normalize_from
+    result = number_normalizer.normalize(call.from)
+    return result if number_normalizer.valid?(result)
 
-  def normalize_from!
-    from = number_normalizer.normalize(call.from)
-    if !number_normalizer.valid?(from)
-      normalized_p_asserted_identity = number_normalizer.normalize(
-        call.variables["variable_sip_p_asserted_identity"]
-      )
-      from = normalized_p_asserted_identity if number_normalizer.valid?(normalized_p_asserted_identity)
-    end
-    self.from = from
-  end
-
-  def normalize_to!
-    self.to = number_normalizer.normalize(call.to)
+    normalized_p_asserted_identity = number_normalizer.normalize(
+      variables["variable_sip_p_asserted_identity"]
+    )
+    number_normalizer.valid?(normalized_p_asserted_identity) ? normalized_p_asserted_identity : result
   end
 end
---
+++ spec/lib/adhearsion-twilio/call_spec.rb
@@ -0,0 +1,67 @@
+require "spec_helper"
+
+module Adhearsion
+  module Twilio
+    RSpec.describe Call do
+      describe "#from" do
+        it "normalizes simple number" do
+          phone_call = build_phone_call(from: "<85516701721@127.0.0.1>")
+
+          call = Call.new(phone_call)
+
+          expect(call.from).to eq("+85516701721")
+        end
+
+        it "normalizes sip_p_asserted_identity number" do
+          phone_call = build_phone_call(
+            from: "<invalid@127.0.0.1>",
+            variables: { "variable_sip_p_asserted_identity" => "85510212050" }
+          )
+
+          call = Call.new(phone_call)
+
+          expect(call.from).to eq("+85510212050")
+        end
+
+        it "normalizes an invalid number" do
+          phone_call = build_phone_call(
+            from: "<invalid@127.0.0.1>",
+            variables: { "variable_sip_p_asserted_identity" => "invalid-sip-address" }
+          )
+
+          call = Call.new(phone_call)
+
+          expect(call.from).to eq("invalid")
+        end
+      end
+
+      describe "#to" do
+        it "normalizes `to` number" do
+          phone_call = build_phone_call(to: "85512345678")
+
+          call = Call.new(phone_call)
+
+          expect(call.to).to eq("+85512345678")
+        end
+      end
+
+      describe "#duration" do
+        it "normalizes the duration" do
+          phone_call = build_phone_call(duration: "26.4")
+
+          call = Call.new(phone_call)
+
+          expect(call.duration).to eq(26)
+        end
+      end
+
+      def build_phone_call(options = {})
+        options.reverse_merge!(
+          variables: {}
+        )
+
+        instance_double(Adhearsion::Call, options)
+      end
+    end
+  end
+end
