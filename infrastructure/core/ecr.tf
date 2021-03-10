6662ba8d2655082061193313e10de31a7d82b9be
Samnang Chhun <samnang.chhun@gmail.com>
2021-03-10 16:24:19 +0700

    Switch to use ECR public registry (#175)

--- infrastructure/core/ecr.tf
+++ infrastructure/core/ecr.tf
@@ -1,15 +1,21 @@
-resource "aws_ecr_repository" "app" {
-  name                 = "somleng-adhearsion"
+resource "aws_ecrpublic_repository" "app" {
+  repository_name = "somleng-adhearsion"
+  provider = aws.us-east-1
 
-  image_scanning_configuration {
-    scan_on_push = true
+  catalog_data {
+    about_text        = "Somleng Adhearsion"
+    architectures     = ["Linux"]
+    description       = "Somleng-Adhearsion is an Adhearsion application compatible with Twilreapi and TwiML. It can be used as a drop-in replacement for Twilio routing calls through local operator, SIP trunk or PBX."
   }
 }
 
-resource "aws_ecr_repository" "nginx" {
-  name                 = "somleng-adhearsion-nginx"
+resource "aws_ecrpublic_repository" "nginx" {
+  repository_name = "somleng-adhearsion-nginx"
+  provider = aws.us-east-1
 
-  image_scanning_configuration {
-    scan_on_push = true
+  catalog_data {
+    about_text        = "Somleng Adhearsion Nginx"
+    architectures     = ["Linux"]
   }
 }
+
