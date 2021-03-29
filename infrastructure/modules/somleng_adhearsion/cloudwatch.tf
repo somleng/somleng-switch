c67331a07036c0a093e4d18b844e5f0e5e1e3683
David Wilkie <dwilkie@gmail.com>
2021-03-29 14:39:24 +0700

    Retain cloudwatch logs for 7 days only

--- infrastructure/modules/somleng_adhearsion/cloudwatch.tf
+++ infrastructure/modules/somleng_adhearsion/cloudwatch.tf
@@ -1,7 +1,9 @@
 resource "aws_cloudwatch_log_group" "nginx" {
   name = "${var.app_identifier}-nginx"
+  retention_in_days = 7
 }
 
 resource "aws_cloudwatch_log_group" "app" {
   name = "${var.app_identifier}-app"
+  retention_in_days = 7
 }
