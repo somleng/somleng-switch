d1af71684b3985aa9c560c39f9c9a578b2a14357
David Wilkie <dwilkie@gmail.com>
2021-08-20 17:03:11 +0700

    Update call platform endpoint

--- infrastructure/production/main.tf
+++ infrastructure/production/main.tf
@@ -1,5 +1,5 @@
-data "aws_ssm_parameter" "twilreapi_services_password" {
-  name = "twilreapi.production.services_password"
+data "aws_ssm_parameter" "somleng_services_password" {
+  name = "somleng.production.services_password"
 }
 
 module "somleng_switch" {
@@ -23,15 +23,15 @@ module "somleng_switch" {
   db_host = data.terraform_remote_state.core_infrastructure.outputs.db.rds_cluster_endpoint
   db_port = data.terraform_remote_state.core_infrastructure.outputs.db.rds_cluster_port
   db_security_group = data.terraform_remote_state.core_infrastructure.outputs.db_security_group.id
-  json_cdr_password_parameter_arn = data.aws_ssm_parameter.twilreapi_services_password.arn
-  json_cdr_url = "https://twilreapi.somleng.org/services/call_data_records"
+  json_cdr_password_parameter_arn = data.aws_ssm_parameter.somleng_services_password.arn
+  json_cdr_url = "https://api.somleng.org/services/call_data_records"
   external_sip_ip = data.terraform_remote_state.core_infrastructure.outputs.nlb_eips[0].public_ip
   external_rtp_ip = data.terraform_remote_state.core_infrastructure.outputs.vpc.nat_public_ips[0]
 
   load_balancer_arn = data.terraform_remote_state.core_infrastructure.outputs.application_load_balancer.arn
   network_load_balancer_arn = data.terraform_remote_state.core_infrastructure.outputs.network_load_balancer.arn
   listener_arn = data.terraform_remote_state.core_infrastructure.outputs.https_listener.arn
-  inbound_sip_trunks_security_group_name = "twilreapi-inbound-sip-trunks"
+  inbound_sip_trunks_security_group_name = "somleng-inbound-sip-trunks"
 
   ecs_appserver_autoscale_min_instances = 1
 }
