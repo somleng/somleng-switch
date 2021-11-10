9098a755ff4999388113054596b502b3fb12f894
David Wilkie <dwilkie@gmail.com>
2021-11-10 10:08:05 +0700

    WIP

--- app/models/utils.rb
+++ app/models/utils.rb
@@ -1,5 +1,12 @@
 module Utils
-  def self.build_dial_string(address)
-    "sofia/external/#{address}"
+  DEFAULT_VARIABLES = {
+    "sip-force-contact" => "NDLB-connectile-dysfunction"
+  }.freeze
+
+  def self.build_dial_string(address, variables = {})
+    variables.merge!(DEFAULT_VARIABLES)
+    prefix = variables.map { |k, v| "#{k}=#{v}" }.join(",")
+    prefix = "{#{prefix}}" if prefix.present?
+    "#{prefix}sofia/external/#{address}"
   end
 end
