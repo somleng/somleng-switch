46639aa10110de4e8166fa843e5af96de18c6cca
Samnang Chhun <samnang.chhun@gmail.com>
2021-11-11 19:20:39 +0700

    Switch different sofia profile

--- app/models/utils.rb
+++ app/models/utils.rb
@@ -1,10 +1,5 @@
 module Utils
-  DEFAULT_VARIABLES = {}.freeze
-
-  def self.build_dial_string(address, variables = {})
-    variables.merge!(DEFAULT_VARIABLES)
-    vars = variables.map { |k, v| "#{k}=#{v}" }.join(",")
-    vars = "{#{prefix}}" if vars.present?
-    "#{vars}sofia/external/#{address}"
+  def self.build_dial_string(address)
+    "sofia/external/#{address}"
   end
 end
