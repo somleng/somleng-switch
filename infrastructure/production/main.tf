271861c888ed124e4f31be769d036b359db34d13
David Wilkie <dwilkie@gmail.com>
2021-11-17 20:16:26 +0700

    Update external SIP IP

--- infrastructure/production/main.tf
+++ infrastructure/production/main.tf
@@ -23,7 +23,7 @@ module "somleng_switch" {
   json_cdr_password_parameter_arn = data.aws_ssm_parameter.somleng_services_password.arn
   json_cdr_url = "https://api.somleng.org/services/call_data_records"
   external_sip_ip = data.terraform_remote_state.core_infrastructure.outputs.vpc.nat_public_ips[0]
-  external_rtp_ip = data.terraform_remote_state.core_infrastructure.outputs.vpc.nat_public_ips[0]
+  external_rtp_ip = data.terraform_remote_state.core_infrastructure.outputs.nlb_eips[0].public_ip
   external_nat_instance_sip_ip = data.terraform_remote_state.core_infrastructure.outputs.nat_instance_ip
   external_nat_instance_rtp_ip = data.terraform_remote_state.core_infrastructure.outputs.nat_instance_ip
 
