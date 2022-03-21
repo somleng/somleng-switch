ff6a20b99241b22a315a3668cd891ecb795346b9
David Wilkie <dwilkie@gmail.com>
2022-03-21 09:11:28 +0700

    WIP

--- infrastructure/package_builder/main.tf
+++ infrastructure/package_builder/main.tf
@@ -9,8 +9,8 @@ data "aws_ami" "debian_latest" {
   }
 }
 
-resource "aws_iam_role" "freeswitch_deb_builder" {
-  name = "freeswitch_deb_builder"
+resource "aws_iam_role" "package_builder" {
+  name = "package_builder"
 
   assume_role_policy = <<EOF
 {
@@ -28,26 +28,26 @@ resource "aws_iam_role" "freeswitch_deb_builder" {
 EOF
 }
 
-resource "aws_iam_instance_profile" "freeswitch_deb_builder" {
-  name = "freeswitch_deb_builder"
-  role = aws_iam_role.freeswitch_deb_builder.name
+resource "aws_iam_instance_profile" "package_builder" {
+  name = "package_builder"
+  role = aws_iam_role.package_builder.name
 }
 
-resource "aws_iam_role_policy_attachment" "freeswitch_deb_builder_ssm" {
-  role       = aws_iam_role.freeswitch_deb_builder.name
+resource "aws_iam_role_policy_attachment" "package_builder_ssm" {
+  role       = aws_iam_role.package_builder.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
-resource "aws_launch_template" "freeswitch_deb_builder" {
-  name_prefix                 = "freeswitch-deb-builder"
+resource "aws_launch_template" "package_builder" {
+  name_prefix                 = "package-builder"
   image_id                    = data.aws_ami.debian_latest.id
   instance_type               = "t4g.small"
 
   iam_instance_profile {
-    name = aws_iam_instance_profile.freeswitch_deb_builder.name
+    name = aws_iam_instance_profile.package_builder.name
   }
 
-  vpc_security_group_ids = [aws_security_group.freeswitch_deb_builder.id]
+  vpc_security_group_ids = [aws_security_group.package_builder.id]
 
   user_data = base64encode(join("\n", [
     "#cloud-config",
@@ -55,12 +55,12 @@ resource "aws_launch_template" "freeswitch_deb_builder" {
       write_files : [
         {
           path : "/opt/builder/build.sh",
-          content : file("${path.module}/templates/freeswitch_deb_builder/build.sh"),
+          content : file("${path.module}/templates/build.sh"),
           permissions : "0755",
         },
         {
           path : "/opt/ssm_agent.sh",
-          content : file("${path.module}/templates/freeswitch_deb_builder/ssm_agent.sh"),
+          content : file("${path.module}/templates/ssm_agent.sh"),
           permissions : "0755",
         },
       ],
@@ -75,40 +75,38 @@ resource "aws_launch_template" "freeswitch_deb_builder" {
   }
 }
 
-resource "aws_security_group" "freeswitch_deb_builder" {
-  name   = "freeswitch-deb-builder"
-  vpc_id = module.vpc.vpc_id
+resource "aws_security_group" "package_builder" {
+  name   = "package-builder"
+  vpc_id = data.terraform_remote_state.core_infrastructure.outputs.vpc.vpc_id
 }
 
-resource "aws_security_group_rule" "freeswitch_deb_builder_egress" {
+resource "aws_security_group_rule" "package_builder_egress" {
   type              = "egress"
   to_port           = 0
   protocol          = "-1"
   from_port         = 0
-  security_group_id = aws_security_group.freeswitch_deb_builder.id
+  security_group_id = aws_security_group.package_builder.id
   cidr_blocks = ["0.0.0.0/0"]
 }
 
-resource "aws_autoscaling_group" "freeswitch_deb_builder" {
-  name                 = "freeswitch-deb-builder"
+resource "aws_autoscaling_group" "package_builder" {
+  name                 = "package-builder"
 
   launch_template {
-    id      = aws_launch_template.freeswitch_deb_builder.id
-    version = aws_launch_template.freeswitch_deb_builder.latest_version
+    id      = aws_launch_template.package_builder.id
+    version = aws_launch_template.package_builder.latest_version
   }
 
-  vpc_zone_identifier = module.vpc.private_subnets
+  vpc_zone_identifier = data.terraform_remote_state.core_infrastructure.outputs.vpc.private_subnets
 
   max_size             = 1
   min_size             = 0
   desired_capacity     = 0
   wait_for_capacity_timeout = 0
 
-  termination_policies = ["OldestInstance"]
-
   tag {
     key                 = "Name"
-    value               = "freeswitch-deb-builder"
+    value               = "package-builder"
     propagate_at_launch = true
   }
 
