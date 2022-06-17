0f0f384d9edd144bb1e62c3ca3fc57fc3d4453b3
David Wilkie <dwilkie@gmail.com>
2022-06-17 17:44:00 +0700

    WIP

--- docker/freeswitch/docker-entrypoint.sh
+++ docker/freeswitch/docker-entrypoint.sh
@@ -4,27 +4,24 @@ set -e
 
 if [ "$1" = 'freeswitch' ]; then
   # Local constants
-  FS_CACHE_DIRECTORY="${FS_CACHE_DIRECTORY:-"/var/lib"}"
-  FREESWITCH_CONTAINER_CONFIG_DIRECTORY="/etc/freeswitch/"
-  FREESWITCH_STORAGE_DIRECTORY="${FS_CACHE_DIRECTORY}/freeswitch/storage"
-  FREESWITCH_TTS_CACHE_DIRECTORY="${FS_CACHE_DIRECTORY}/freeswitch/tts_cache"
+  FS_CONTAINER_CONFIG_DIRECTORY="/etc/freeswitch/"
 
-  FREESWITCH_CONTAINER_BINARY="/usr/bin/freeswitch"
-  FREESWITCH_USER="freeswitch"
-  FREESWITCH_GROUP="daemon"
+  FS_CONTAINER_BINARY="/usr/bin/freeswitch"
+  FS_USER="freeswitch"
+  FS_GROUP="daemon"
+
+  export_fs_env_vars "$FS_CONTAINER_CONFIG_DIRECTORY/env.xml"
 
   # Setup directories
 
-  for directory in "$FREESWITCH_CONTAINER_CONFIG_DIRECTORY" "$FREESWITCH_STORAGE_DIRECTORY" "$FREESWITCH_TTS_CACHE_DIRECTORY"
+  for directory in "$FS_CONTAINER_CONFIG_DIRECTORY" "$FS_STORAGE_DIRECTORY" "$FS_TTS_CACHE_DIRECTORY"
   do
     mkdir -p "$directory"
-    chown -R "$FREESWITCH_USER:$FREESWITCH_GROUP" "$directory"
+    chown -R "$FS_USER:$FS_GROUP" "$directory"
   done
 
-  export_fs_env_vars "$FREESWITCH_CONTAINER_CONFIG_DIRECTORY/env.xml"
-
   # execute FreeSWITCH
-  exec "$FREESWITCH_CONTAINER_BINARY" -u "$FREESWITCH_USER" -g "$FREESWITCH_GROUP" -nonat -storage "$FREESWITCH_STORAGE_DIRECTORY"
+  exec "$FS_CONTAINER_BINARY" -u "$FS_USER" -g "$FS_GROUP" -nonat -storage "$FS_STORAGE_DIRECTORY"
 fi
 
 exec "$@"
