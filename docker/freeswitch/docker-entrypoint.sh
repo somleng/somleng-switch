c4cc95452d88cfd3851fcaf8980f1dae149d2c37
David Wilkie <dwilkie@gmail.com>
2022-06-17 17:15:07 +0700

    WIP

--- docker/freeswitch/docker-entrypoint.sh
+++ docker/freeswitch/docker-entrypoint.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# Local constants
-FS_CACHE_DIRECTORY="${FS_CACHE_DIRECTORY:-"/var/lib"}"
-FREESWITCH_CONTAINER_CONFIG_DIRECTORY="/etc/freeswitch/"
-FREESWITCH_STORAGE_DIRECTORY="${FS_CACHE_DIRECTORY}/freeswitch/storage"
-FREESWITCH_TTS_CACHE_DIRECTORY="${FS_CACHE_DIRECTORY}/freeswitch/tts_cache"
-
-FREESWITCH_CONTAINER_BINARY="/usr/bin/freeswitch"
-FREESWITCH_USER="freeswitch"
-FREESWITCH_GROUP="daemon"
-
 set -e
 
 if [ "$1" = 'freeswitch' ]; then
+  # Local constants
+  FS_CACHE_DIRECTORY="${FS_CACHE_DIRECTORY:-"/var/lib"}"
+  FREESWITCH_CONTAINER_CONFIG_DIRECTORY="/etc/freeswitch/"
+  FREESWITCH_STORAGE_DIRECTORY="${FS_CACHE_DIRECTORY}/freeswitch/storage"
+  FREESWITCH_TTS_CACHE_DIRECTORY="${FS_CACHE_DIRECTORY}/freeswitch/tts_cache"
+
+  FREESWITCH_CONTAINER_BINARY="/usr/bin/freeswitch"
+  FREESWITCH_USER="freeswitch"
+  FREESWITCH_GROUP="daemon"
+
   # Setup directories
 
   for directory in "$FREESWITCH_CONTAINER_CONFIG_DIRECTORY" "$FREESWITCH_STORAGE_DIRECTORY" "$FREESWITCH_TTS_CACHE_DIRECTORY"
