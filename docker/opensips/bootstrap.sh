d86669384f60fc7036e9d73115896b1a04ec23f1
David Wilkie <dwilkie@gmail.com>
2022-07-27 11:20:38 +0700

    WIP

--- docker/opensips/bootstrap.sh
+++ docker/opensips/bootstrap.sh
@@ -7,7 +7,7 @@
 set -e
 
 if [ "$1" = 'create_db' ]; then
-  DATABASE_MODULES="${DATABASE_MODULES:="load_balancer"}"
+  DATABASE_MODULES="${DATABASE_MODULES:="dialog load_balancer"}"
 
   cat <<-EOT > /etc/opensips-cli.cfg
 	[default]
@@ -23,11 +23,11 @@ elif [ "$1" = 'add_module' ]; then
 	database_admin_url: $DATABASE_URL
 	EOT
 
-  for module in `cat $DATABASE_MODULES`; do
+  for module in $DATABASE_MODULES
+  do
     echo $module
+    opensips-cli -x database add "$module"
   done
-
-  # opensips-cli -x database add dialog
+else
+  exec "$@"
 fi
-
-exec "$@"
