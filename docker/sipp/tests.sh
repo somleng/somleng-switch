73d075013567121174018911dfcc973831b872b0
David Wilkie <dwilkie@gmail.com>
2022-08-05 13:13:40 +0700

    WIP

---
+++ docker/sipp/tests.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Usage
+# Runs tests in the tests directory relative to this script.
+
+set -e
+
+current_dir=$(dirname "$(readlink -f "$0")")
+
+for f in $current_dir/tests/*.sh; do
+  sh "$f"
+done
---
+++ docker/sipp/tests/smart_inbound_test.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+
+current_dir=$(dirname "$(readlink -f "$0")")
+scenario=$current_dir/../scenarios/smart_inbound.xml
+
+sipp -sf $scenario opensips:5060 -s 1234 -m 1 -trace_msg > /dev/null
+
+test_string="c=IN IP4 13.250.230.15" # EXT_RTP_IP
+
+if ! grep -q "$test_string" smart_inbound_*_messages.log; then
+  cat <<-EOT
+	Error:
+	$test_string not found in:
+	`cat smart_inbound_*_messages.log`
+	EOT
+  exit 1
+fi
