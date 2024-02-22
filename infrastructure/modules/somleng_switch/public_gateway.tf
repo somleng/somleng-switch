dd67173aeb015bb52a71d6ea0d92007a0a603453
David Wilkie <dwilkie@gmail.com>
2024-02-22 22:09:46 +0700

    Configure ECS instance draining

--- infrastructure/modules/somleng_switch/public_gateway.tf
+++ infrastructure/modules/somleng_switch/public_gateway.tf
@@ -16,6 +16,7 @@ resource "aws_ecs_capacity_provider" "public_gateway" {
   auto_scaling_group_provider {
     auto_scaling_group_arn         = module.public_gateway_container_instances.autoscaling_group.arn
     managed_termination_protection = "ENABLED"
+    managed_draining = "ENABLED"
 
     managed_scaling {
       maximum_scaling_step_size = 1000
