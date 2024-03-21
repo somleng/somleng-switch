618fedf98fbf6bfaec9f69aad02aab7e4e7d025f
David Wilkie <dwilkie@gmail.com>
2024-03-21 22:20:44 +0700

    WIP

--- components/testing/docker-entrypoint.sh
+++ components/testing/docker-entrypoint.sh
@@ -5,7 +5,7 @@ if [ "$1" = 'start' ]; then
   WS_SERVER_PORT="${WS_SERVER_PORT:="3001"}"
 
   sipp -sf ./scenarios/uas.xml -bg -trace_msg
-  nohup node ./support/ws_server/server.js "$WS_SERVER_AUDIO_OUTPUT" --port "$WS_SERVER_PORT" > ws-server.log &
+  nohup node ./support/ws_server/server_twilio.js "$WS_SERVER_AUDIO_OUTPUT" --port "$WS_SERVER_PORT" > ws-server.log &
   tail -f /dev/null
 fi
 
