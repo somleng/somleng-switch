aa9310b56607ac0698ef4deb8871f64c78afbb6f
Jacob Stahlbaum <jstahlbaum@corp.fibernetics.ca>
2024-03-21 16:03:35 -0400

    Added new events when twilio messages are generated. Changed api to base64 encode of metadata

--- components/freeswitch/src/mod/mod_twilio_stream/include/mod_twilio_stream.h
+++ components/freeswitch/src/mod/mod_twilio_stream/include/mod_twilio_stream.h
@@ -12,29 +12,34 @@
 #define MAX_WS_URL_LEN (512)
 #define MAX_PATH_LEN (4096)
 
-#define EVENT_TRANSCRIPTION   "mod_twilio_stream::transcription"
-#define EVENT_TRANSFER        "mod_twilio_stream::transfer"
-#define EVENT_PLAY_AUDIO      "mod_twilio_stream::play_audio"
-#define EVENT_KILL_AUDIO      "mod_twilio_stream::kill_audio"
-#define EVENT_DISCONNECT      "mod_twilio_stream::disconnect"
-#define EVENT_ERROR           "mod_twilio_stream::error"
+#define EVENT_BUFFER_OVERRUN "mod_twilio_stream::buffer_overrun"
+#define EVENT_CONNECT_FAIL "mod_twilio_stream::connect_failed"
 #define EVENT_CONNECT_SUCCESS "mod_twilio_stream::connect"
-#define EVENT_CONNECT_FAIL    "mod_twilio_stream::connect_failed"
-#define EVENT_BUFFER_OVERRUN  "mod_twilio_stream::buffer_overrun"
-#define EVENT_JSON            "mod_twilio_stream::json"
+#define EVENT_DISCONNECT "mod_twilio_stream::disconnect"
+#define EVENT_DTMF "mod_twilio_stream::dtmf"
+#define EVENT_ERROR "mod_twilio_stream::error"
+#define EVENT_MARK "mod_twilio_stream::mark"
+#define EVENT_SOCKET_CLEAR "mod_twilio_stream::socket_clear"
+#define EVENT_SOCKET_MARK "mod_twilio_stream::socket_mark"
+#define EVENT_START "mod_twilio_stream::start"
+//Make sure the above and below stays in sync!!!
+#define ALL_EVENTS {EVENT_BUFFER_OVERRUN,EVENT_CONNECT_FAIL,EVENT_CONNECT_SUCCESS,EVENT_DISCONNECT,EVENT_DTMF,EVENT_ERROR,EVENT_MARK,EVENT_SOCKET_CLEAR,EVENT_SOCKET_MARK, EVENT_START}
+
 
 #define BUG_NAME "__mod_twilio_stream"
 
-struct playout {
+struct playout
+{
   char *file;
-  struct playout* next;
+  struct playout *next;
 };
 
-typedef void (*responseHandler_t)(switch_core_session_t* session, const char* eventName, char* json);
+typedef void (*responseHandler_t)(switch_core_session_t *session, const char *eventName, const char *json);
 
-struct private_data {
-	switch_mutex_t *mutex;
-	char sessionId[MAX_SESSION_ID];
+struct private_data
+{
+  switch_mutex_t *mutex;
+  char sessionId[MAX_SESSION_ID];
   SpeexResamplerState *resampler_out;
   SpeexResamplerState *resampler_in;
   responseHandler_t responseHandler;
@@ -44,13 +49,13 @@ struct private_data {
   char host[MAX_WS_URL_LEN];
   unsigned int port;
   char path[MAX_PATH_LEN];
-  int sampling; //call sample rate
-  int desiredSampling; //twilio sample rate
-  int  channels;
+  int sampling;        // call sample rate
+  int desiredSampling; // twilio sample rate
+  int channels;
   unsigned int id;
-  int buffer_overrun_notified:1;
-  int audio_paused:1;
-  int graceful_shutdown:1;
+  int buffer_overrun_notified : 1;
+  int audio_paused : 1;
+  int graceful_shutdown : 1;
 };
 
 typedef struct private_data private_t;
--- components/freeswitch_event_logger/app.go
+++ components/freeswitch_event_logger/app.go
@@ -21,12 +21,34 @@ func (nopLogger) Info(string) error    { return nil }
 func (nopLogger) Notice(string) error  { return nil }
 func (nopLogger) Warning(string) error { return nil }
 
+const EVENT_DISCONNECT = "mod_twilio_stream::disconnect"
+const EVENT_ERROR = "mod_twilio_stream::error"
+const EVENT_CONNECT_SUCCESS = "mod_twilio_stream::connect"
+const EVENT_START = "mod_twilio_stream::start"
+const EVENT_STOP = "mod_twilio_stream::stop"
+const EVENT_DTMF  ="mod_twilio_stream::dtmf"
+const EVENT_MARK = "mod_twilio_stream::mark"
+const EVENT_SOCKET_MARK = "mod_twilio_stream::socket_mark"
+const EVENT_SOCKET_CLEAR = "mod_twilio_stream::socket_clear"
+
 // Formats the event as map and prints it out
 func logHeartbeat(eventStr string, connIdx int) {
 	// Format the event from string into Go's map type
 	eventMap := fsock.FSEventStrToMap(eventStr, []string{})
 	jsonString, _ := json.Marshal(eventMap)
 	fmt.Println(string(jsonString))
+}
+
+func customEventHantler(eventStr string, connIdx int) {
+	eventMap := fsock.FSEventStrToMap(eventStr, []string{})
+	switch eventMap["Event-Subclass"] {
+    case EVENT_CONNECT_SUCCESS:
+        fmt.Println("CONNECT")
+    case EVENT_DISCONNECT:
+        fmt.Println("DISCONNECT")
+	default:
+		fmt.Println("Unhandled Event Type:" + eventMap["Event-Subclass"])
+    }
 }
 
 func fibDuration(durationUnit, maxDuration time.Duration) func() time.Duration {
@@ -45,9 +67,10 @@ func main() {
 	// Filters
 	evFilters := make(map[string][]string)
 	evFilters["Event-Name"] = append(evFilters["Event-Name"], "HEARTBEAT")
-
+	evFilters["Event-Name"] = append(evFilters["Event-Name"], "CUSTOM")
 	evHandlers := map[string][]func(string, int){
 		"HEARTBEAT": {logHeartbeat},
+		"ALL": {customEventHantler},
 	}
 
 	event_socket_host := os.Getenv("EVENT_SOCKET_HOST")
