34ffb4468ad273fb55d917a01928fb39b1506286
Jacob Stahlbaum <jstahlbaum@corp.fibernetics.ca>
2024-03-25 21:57:10 -0400

    Added very rough unit test

--- components/testing/support/ws_server/server_twilio.js
+++ components/testing/support/ws_server/server_twilio.js
@@ -16,55 +16,72 @@ const wss = new WebSocket.Server({
 
 
 class AudioTestStream {
-  audioData = new Buffer();
+  audioData = Buffer.alloc(0);
   chunkMs = 80;
   sampleRate = 8000;
-  chunkSize = sampleRate * (chunkMs / 1000.0);
+  chunkSize = this.sampleRate * (this.chunkMs / 1000.0);
+  streamSid = "";
+  saveAudio = false;
+  audioPath = "";
 
-  initializeAudio() {
-    this.audioData = new Buffer();
+  initializeAudio(streamSid) {
+    this.audioData = Buffer.alloc(0);
+    this.streamSid = streamSid;
   }
 
-  streamStoredAudio(ws, streamSid) {
-    for (let i = 0; i < this.audioData.length / this.chunkSize; i++) {
-      const base64Data = this.audioData.subarray(i * this.chunkSize, (i + 1) * this.chunkSize).toString('base64');
-      const msg = makeOutboundSample(base64Data, streamSid)
-
-      if (ws)
-        ws.send(JSON.stringify(msg))
+  streamStoredAudio(ws) {
+    console.log(`streamStoredAudio start sending data LEN: ${this.audioData.length}`);
+    if(this.saveAudio && this.audioPath) {
+      let wstream = fs.createWriteStream(audioPath);
+      wstream.write(this.audioData)
+      wstream.close()
     }
 
-    const msg = makeMark(base64Data, streamSid)
-    if (ws)
+    for (let i = 0; i < this.audioData.length / this.chunkSize; i++) {
+      const base64Data = this.audioData.subarray(i * this.chunkSize, (i + 1) * this.chunkSize).toString('base64');
+      const msg = this.makeOutboundSample(base64Data)
+
+      if (ws) {
+        console.log(`${JSON.stringify(msg)}`)
+        ws.send(JSON.stringify(msg))
+      }
+    }
+
+    const msg = this.makeMark()
+    if (ws) {
+      console.log(`${JSON.stringify(msg)}`)
       ws.send(JSON.stringify(msg))
+    }
   }
 
   appendAudio(data) {
-    this.audioData = Buffer.concat(this.audioData, data)
+    console.log(`Append audio ${this.audioData.length} -> ${data.length}`)
+    this.audioData = Buffer.concat([this.audioData, data])
   }
 
-  makeOutboundSample(base64, streamSid) {
+  makeOutboundSample(base64) {
     return {
       "event": "media",
       "media": {
         "payload": base64,
       },
-      "streamSid": streamSid
+      "streamSid": this.streamSid
     }
   }
 
-  makeMark(streamSid) {
+  makeMark() {
     return {
       "event": "mark",
       "mark": {
         "name": "audio"
       },
-      "streamSid": streamSid
+      "streamSid": this.streamSid
     }
   }
 }
 
 const audioStream = new AudioTestStream();
+let isRecord = true;
 wss.on("connection", (ws, req) => {
   ws.on("message", (message) => {
     if (typeof message === "string") {
@@ -74,13 +91,17 @@ wss.on("connection", (ws, req) => {
       try {
         const json = JSON.parse(message);
         if (json['event'] === 'start') {
+          isRecord = true;
           console.log(`Starting recieve data`);
-          audioStream.initializeAudio()
+          audioStream.initializeAudio(json['streamSid'])
         } else if (json['event'] === 'dtmf') {
           console.log(`Start sending stored data`);
+          isRecord = false
           audioStream.streamStoredAudio(ws);
         } else if (json['event'] === 'media') {
-          audioStream.appendAudio(Buffer.from(b64string, 'base64'))
+          const b64string = json['media']["payload"]
+          if(isRecord)
+            audioStream.appendAudio(Buffer.from(b64string, 'base64'))
         } else if (json['event'] === 'mark') {
           console.log(`Closing Stream`);
           ws.close();
--- components/testing/tests/public_gateway/connect_stream_test.sh
+++ components/testing/tests/public_gateway/connect_stream_test.sh
@@ -19,13 +19,28 @@ create_address_entry $(hostname -i)
 reload_opensips_tables
 
 # start tcpdump here in background
+( tcpdump -Xvv -i eth0 -s0 -w capture.pcap ) &
+TCPDUMP_PID=$!
 
 sipp -sf $scenario public_gateway:5060 -key username "+855715100850" -s 2222 -m 1 -trace_msg > /dev/null
 
+echo "Killing TCPDUMP after sipp"
+
 #kill tcpdump
+kill $TCPDUMP_PID 
+
+#extact audio
+tshark -n -r capture.pcap -2 -R rtp -T fields -e rtp.payload | tr -d '\n',':' | xxd -r -p >call.rtp
+sox -t al -r 8000 -c 1 call.rtp call.wav
+
+#asset md5hash of wav == expected wav file
+expectedFile=$current_dir/../../scenarios/files/expected.wav
 
 reset_db
 
+if [[ "$(md5sum call.wav)" != "$(md5sum $expectedFile)" ]]; then
+	exit 1
+fi
 # Assert correct IP in SDP
 if ! assert_in_file $log_file "c=IN IP4 $media_server"; then
 	exit 1
