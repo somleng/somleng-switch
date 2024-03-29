8766c6ebf1d6fcce437e3b65f3b45521f6f50ad2
David Wilkie <dwilkie@gmail.com>
2024-03-29 16:34:42 +0700

    WIP

--- components/testing/tests/public_gateway/outbound_test.sh
+++ components/testing/tests/public_gateway/outbound_test.sh
@@ -7,7 +7,10 @@ source $current_dir/support/test_helpers.sh
 source $current_dir/../support/test_helpers.sh
 
 log_file=$(find . -type f -iname "uas_*_messages.log")
-cat /dev/null > $log_file
+
+if [ -f "$log_file" ]; then
+  cat /dev/null > $log_file
+fi
 
 uas="$(dig +short testing)"
 media_server="$(dig +short freeswitch)"
