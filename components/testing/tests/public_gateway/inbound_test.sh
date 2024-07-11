e5b1b738a77039d7fcca4b64c969a139fbaa9da4
David Wilkie <dwilkie@gmail.com>
2024-07-11 17:39:57 +0700

    Fix integration tests

--- components/testing/tests/public_gateway/inbound_test.sh
+++ components/testing/tests/public_gateway/inbound_test.sh
@@ -9,7 +9,7 @@ source $current_dir/../support/test_helpers.sh
 scenario=$current_dir/../../scenarios/smart_inbound.xml
 
 log_file="smart_inbound_*_messages.log"
-rm -f "$log_file"
+rm -f $log_file
 
 media_server="$(dig +short freeswitch)"
 public_gateway="$(dig +short public_gateway)"
