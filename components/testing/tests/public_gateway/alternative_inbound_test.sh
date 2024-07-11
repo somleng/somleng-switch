6f01d5c4798a7cf8d9591e91d283c56ca311490f
David Wilkie <dwilkie@gmail.com>
2024-07-11 17:30:41 +0700

    Set correct Record-Route Port (#674)

--- components/testing/tests/public_gateway/alternative_inbound_test.sh
+++ components/testing/tests/public_gateway/alternative_inbound_test.sh
@@ -8,15 +8,25 @@ source $current_dir/../support/test_helpers.sh
 
 scenario=$current_dir/../../scenarios/zamtel_inbound.xml
 
+log_file="zamtel_inbound_*_messages.log"
+rm -f "$log_file"
+
 reset_db
 create_load_balancer_entry "gwalt" "5080"
 create_address_entry $(hostname -i)
 reload_opensips_tables
 
-rm -f zamtel_inbound_*_messages.log
+public_gateway="$(dig +short public_gateway)"
+
 sipp -sf $scenario public_gateway:5080 -s 7888 -m 1 -trace_msg > /dev/null
 reset_db
 
-if ! assert_in_file "zamtel_inbound_*_messages.log" "c=IN IP4 18.141.245.230"; then
+# Assert correct IP in SDP
+if ! assert_in_file "$log_file" "c=IN IP4 18.141.245.230"; then
+	exit 1
+fi
+
+# Assert correct Port in RR
+if ! assert_in_file "$log_file" "Record-Route: <sip:$public_gateway:5080"; then
 	exit 1
 fi
--- components/testing/tests/public_gateway/inbound_test.sh
+++ components/testing/tests/public_gateway/inbound_test.sh
@@ -9,9 +9,10 @@ source $current_dir/../support/test_helpers.sh
 scenario=$current_dir/../../scenarios/smart_inbound.xml
 
 log_file="smart_inbound_*_messages.log"
-rm -f $log_file
+rm -f "$log_file"
 
 media_server="$(dig +short freeswitch)"
+public_gateway="$(dig +short public_gateway)"
 
 reset_db
 create_load_balancer_entry "gw" "5060"
@@ -23,6 +24,11 @@ sipp -sf $scenario public_gateway:5060 -s 1234 -m 1 -trace_msg > /dev/null
 reset_db
 
 # Assert correct IP in SDP
-if ! assert_in_file $log_file "c=IN IP4 $media_server"; then
+if ! assert_in_file "$log_file" "c=IN IP4 $media_server"; then
+	exit 1
+fi
+
+# Assert correct Port in RR
+if ! assert_in_file "$log_file" "Record-Route: <sip:$public_gateway:5060"; then
 	exit 1
 fi
