11010249333d6d1ca2fd32292dd1cb6964f6c371
David Wilkie <dwilkie@gmail.com>
2024-07-12 21:44:32 +0700

    Improve E2E tests

--- components/testing/infrastructure/tests/public_gateway_alternative_inbound_test.sh
+++ components/testing/infrastructure/tests/public_gateway_alternative_inbound_test.sh
@@ -9,9 +9,9 @@ HOSTNAME=$(cat /etc/hostname)
 
 read -p "1. Add (or modify) a SIP trunk on Somleng with the following Source IP for Inbound Dialing: $AWS_PUBLIC_IP. Press any key when done."
 read -p "2. Configure the number: $DESTINATION_NUMBER on Somleng: Press any key when done."
-read -p "3. Start TCP dump. In another terminal run the following: sudo docker run -it --rm --net container:$HOSTNAME nicolaka/netshoot followed by tcpdump -Xvv -i eth0 -s0 -w capture.pcap. Press any key when done."
+read -p "3. Start TCP dump. In another terminal run the following: sudo docker run -it --rm --net container:$HOSTNAME nicolaka/netshoot followed by tcpdump -Xvv -i ens5 -s0 -w capture.pcap. Press any key when done."
 
 log_file="uac_*_messages.log"
 rm -f $log_file
 
-sipp -sf scenarios/uac.xml 15.197.218.231:5080 -key username "+855715100850" -s 1234 -m 1 -trace_msg > /dev/null
+sipp -sf scenarios/uac.xml 15.197.218.231:5080 -d 20000 -mi "$AWS_PUBLIC_IP" --key username "+855715100850" -s 1234 -m 1 -min_rtp_port 10000 -max_rtp_port 50000 -trace_msg > /dev/null
