f8e8d4df83db078753eda391a00b0dc8ee953f71
David Wilkie <dwilkie@gmail.com>
2024-07-13 14:56:35 +0700

    Add more E2E tests

--- components/testing/infrastructure/tests/public_gateway_inbound_ga_5080_test.sh
+++ components/testing/infrastructure/tests/public_gateway_inbound_ga_5080_test.sh
@@ -5,11 +5,10 @@ set -e
 TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
 AWS_PUBLIC_IP=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/public-ipv4)
 DESTINATION_NUMBER="1234"
-HOSTNAME=$(cat /etc/hostname)
 
 read -p "1. Add (or modify) a SIP trunk on Somleng with the following Source IP for Inbound Dialing: $AWS_PUBLIC_IP. Press any key when done."
 read -p "2. Configure the number: $DESTINATION_NUMBER on Somleng: Press any key when done."
-read -p "3. Start TCP dump. In another terminal run the following: sudo docker run -it --rm --net container:$HOSTNAME nicolaka/netshoot followed by tcpdump -Xvv -i ens5 -s0 -w capture.pcap. Press any key when done."
+read -p "3. Start TCP dump. In another terminal run the following: sudo docker run -it --rm --net container:cid nicolaka/netshoot followed by tcpdump -Xvv -i ens5 -s0 -w capture.pcap. Press any key when done."
 
 log_file="uac_*_messages.log"
 rm -f $log_file
