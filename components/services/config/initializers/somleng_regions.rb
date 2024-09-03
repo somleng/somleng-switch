9afc4edcb5f67dc749ab573576f16105b128b1a2
David Wilkie <dwilkie@gmail.com>
2024-09-03 22:23:55 +0700

    Fix Yaml loading

--- components/services/config/initializers/somleng_regions.rb
+++ components/services/config/initializers/somleng_regions.rb
@@ -1,6 +1,6 @@
 require "json"
 
 SomlengRegions.configure do |config|
-  config.region_data = JSON.parse(AppSettings.fetch(:region_data))
+  config.region_data = AppSettings.fetch(:region_data)
   config.stub_regions = AppSettings.fetch(:stub_regions)
 end
