afc7355c6cfdf1d070563677c64d6350561bdb4b
David Wilkie <dwilkie@gmail.com>
2024-09-24 23:25:27 +0700

    WIP

--- infrastructure/staging/public_gateway.tf
+++ infrastructure/staging/public_gateway.tf
@@ -1,23 +1,17 @@
 module "public_gateway" {
   source = "../modules/public_gateway"
 
-  identifier      = var.public_gateway_identifier
-  app_environment = var.app_environment
-
-  aws_region = var.aws_default_region
-  vpc        = data.terraform_remote_state.core_infrastructure.outputs.hydrogen_region.vpc
-
-  ecs_cluster = aws_ecs_cluster.this
-
-  app_image       = data.terraform_remote_state.core.outputs.public_gateway_ecr_repository.this.repository_url
-  scheduler_image = data.terraform_remote_state.core.outputs.gateway_scheduler_ecr_repository.this.repository_url
-
-  min_tasks = 0
-  max_tasks = 2
-
-  sip_port             = var.sip_port
-  sip_alternative_port = var.sip_alternative_port
-
+  identifier            = var.public_gateway_identifier
+  app_environment       = var.app_environment
+  aws_region            = var.aws_default_region
+  vpc                   = data.terraform_remote_state.core_infrastructure.outputs.hydrogen_region.vpc
+  ecs_cluster           = aws_ecs_cluster.this
+  app_image             = data.terraform_remote_state.core.outputs.public_gateway_ecr_repository.this.repository_url
+  scheduler_image       = data.terraform_remote_state.core.outputs.gateway_scheduler_ecr_repository.this.repository_url
+  min_tasks             = 0
+  max_tasks             = 2
+  sip_port              = var.sip_port
+  sip_alternative_port  = var.sip_alternative_port
   db_security_group     = data.terraform_remote_state.core_infrastructure.outputs.db_security_group
   db_password_parameter = data.terraform_remote_state.core_infrastructure.outputs.db_master_password_parameter
   db_name               = var.public_gateway_db_name
