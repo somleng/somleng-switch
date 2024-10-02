559d411233548e6cb9cd8e5d68d1b7befd964a62
David Wilkie <dwilkie@gmail.com>
2024-10-02 13:03:55 +0700

    Add event rules for media proxy and client gateway

--- infrastructure/staging/media_proxy.tf
+++ infrastructure/staging/media_proxy.tf
@@ -4,10 +4,11 @@ module "media_proxy" {
   identifier      = var.media_proxy_identifier
   app_environment = var.app_environment
 
-  aws_region  = var.aws_default_region
-  vpc         = data.terraform_remote_state.core_infrastructure.outputs.hydrogen_region.vpc
-  ecs_cluster = aws_ecs_cluster.this
-  app_image   = data.terraform_remote_state.core.outputs.media_proxy_ecr_repository.this.repository_url
+  aws_region        = var.aws_default_region
+  vpc               = data.terraform_remote_state.core_infrastructure.outputs.hydrogen_region.vpc
+  ecs_cluster       = aws_ecs_cluster.this
+  app_image         = data.terraform_remote_state.core.outputs.media_proxy_ecr_repository.this.repository_url
+  services_function = module.services
 
   min_tasks = 0
   max_tasks = 2
