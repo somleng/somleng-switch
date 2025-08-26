46b651a6d961b5a24edcd3fc342961503f829a60
David Wilkie <dwilkie@gmail.com>
2025-08-26 19:54:56 +0700

    WIP

--- components/testing/tests/public_gateway/billing_engine_ping_test.sh
+++ components/testing/tests/public_gateway/billing_engine_ping_test.sh
@@ -6,7 +6,7 @@ current_dir=$(dirname "$(readlink -f "$0")")
 source $current_dir/support/test_helpers.sh
 source $current_dir/../support/test_helpers.sh
 
-response=$(curl -s -X POST http://billing-engine:2080/jsonrpc \
+response=$(curl -s -X POST http://billing-engine:$BILLING_ENGINE_HTTP_PORT/jsonrpc \
   -H "Content-Type: application/json" \
   -d '{"jsonrpc":"2.0","id":1,"method":"ApierV2.Ping","params":[]}')
 
