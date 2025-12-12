542f889032691618075a96063a3ebc6ef043f828
David Wilkie <dwilkie@gmail.com>
2025-12-12 19:03:23 +0700

    WIP

--- infrastructure/staging/public_gateway.tf
+++ infrastructure/staging/public_gateway.tf
@@ -8,7 +8,7 @@ module "public_gateway" {
   ecs_cluster           = aws_ecs_cluster.this
   app_image             = data.terraform_remote_state.core.outputs.public_gateway_ecr_repository.this.repository_url
   scheduler_image       = data.terraform_remote_state.core.outputs.gateway_scheduler_ecr_repository.this.repository_url
-  min_tasks             = 1
+  min_tasks             = 0
   max_tasks             = 2
   sip_port              = var.sip_port
   sip_alternative_port  = var.sip_alternative_port
