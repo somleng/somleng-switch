63735e7955ac085efb88d1a72a60f5ed3c152a20
Samnang Chhun <samnang.chhun@gmail.com>
2026-01-05 17:51:00 +0700

    Fix cgrates production deployment (#1502)

--- infrastructure/production/rating_engine.tf
+++ infrastructure/production/rating_engine.tf
@@ -10,11 +10,11 @@ module "rating_engine" {
   app_image = data.terraform_remote_state.core.outputs.rating_engine_ecr_repository.this.repository_url
 
   stordb_dbname                 = "cgrates"
-  stordb_user                   = data.terraform_remote_state.core_infrastructure.outputs.db_staging.this.master_username
-  stordb_host                   = data.terraform_remote_state.core_infrastructure.outputs.db_staging.this.endpoint
-  stordb_port                   = data.terraform_remote_state.core_infrastructure.outputs.db_staging.this.port
-  stordb_password_parameter_arn = data.terraform_remote_state.core_infrastructure.outputs.db_staging.master_password_parameter.arn
-  stordb_security_group         = data.terraform_remote_state.core_infrastructure.outputs.db_staging.security_group.id
+  stordb_user                   = data.terraform_remote_state.core_infrastructure.outputs.db.this.master_username
+  stordb_host                   = data.terraform_remote_state.core_infrastructure.outputs.db.this.endpoint
+  stordb_port                   = data.terraform_remote_state.core_infrastructure.outputs.db.this.port
+  stordb_password_parameter_arn = data.terraform_remote_state.core_infrastructure.outputs.db.master_password_parameter.arn
+  stordb_security_group         = data.terraform_remote_state.core_infrastructure.outputs.db.security_group.id
   datadb_cache                  = module.redis
 
   lb_rule_index = 50
