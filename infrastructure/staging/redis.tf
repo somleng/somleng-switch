401cb2c04192e94b566b420e5b1ca8affc9d2bb7
Samnang Chhun <samnang.chhun@gmail.com>
2026-02-05 20:04:16 +0700

    Switch valkey from serverless to cluster

--- infrastructure/staging/redis.tf
+++ infrastructure/staging/redis.tf
@@ -1,6 +1,9 @@
 module "redis" {
   source = "../modules/redis"
 
-  identifier = "switch-staging"
-  vpc        = data.terraform_remote_state.core_infrastructure.outputs.hydrogen_region.vpc
+  identifier                 = "switch-staging"
+  vpc                        = data.terraform_remote_state.core_infrastructure.outputs.hydrogen_region.vpc
+  node_type                  = "cache.t4g.micro"
+  automatic_failover_enabled = false
+  num_cache_clusters         = 1
 }
