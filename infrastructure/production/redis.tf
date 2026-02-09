710c8cba4d4c0e8cd6d55ac21df0f9c5bde1eec9
David Wilkie <dwilkie@gmail.com>
2026-02-09 16:36:18 +0700

    Add production infrastructure for valkey

--- infrastructure/production/redis.tf
+++ infrastructure/production/redis.tf
@@ -1,6 +1,9 @@
 module "redis" {
   source = "../modules/redis"
 
-  identifier = "switch"
-  vpc        = data.terraform_remote_state.core_infrastructure.outputs.hydrogen_region.vpc
+  identifier                 = "switch"
+  vpc                        = data.terraform_remote_state.core_infrastructure.outputs.hydrogen_region.vpc
+  node_type                  = "cache.t4g.small"
+  automatic_failover_enabled = true
+  num_cache_clusters         = 2
 }
