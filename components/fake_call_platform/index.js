1e6a10cef6b3192a51d052b7efd23ac1b58a9c8d
Samnang Chhun <samnang.chhun@gmail.com>
2026-02-27 18:03:47 +0700

    WIP

--- components/fake_call_platform/index.js
+++ components/fake_call_platform/index.js
@@ -1,7 +1,7 @@
 import { createServer } from 'node:http'
 import crypto from 'node:crypto'
 
-const hostname = '127.0.0.1'
+const hostname = '0.0.0.0'
 const port = 3000
 
 const { CONNECT_WS_SERVER_URL, AUDIO_FILE_URL } = process.env
@@ -29,15 +29,45 @@ const TEST_NUMBERS = {
   },
 }
 
-const parseBody = async (req) => {
-  const body = await new Response(req).text()
-  return body ? JSON.parse(body) : {}
-}
-
 const server = createServer(async (req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'application/json')
+  try {
+    res.statusCode = 201
+    res.setHeader('Content-Type', 'application/json')
 
+    switch(req.url) {
+      case '/health_checks':
+        res.statusCode = 200
+      case '/services/phone_call_events':
+      case '/services/tts_events':
+      case '/services/media_stream_events':
+        res.end(JSON.stringify({}))
+        break
+      case '/services/inbound_phone_calls':
+        await handleInboundPhoneCalls(req, res)
+        break
+      case '/services/outbound_phone_calls':
+        await handleOutboundPhoneCalls(req, res)
+        break
+      case '/services/media_streams':
+        await handleMediaStreams(req, res)
+        break
+      default:
+        if (req.url.startsWith('/services/recordings')) {
+          await handleRecordings(req, res)
+          return
+        }
+
+        res.statusCode = 404
+        res.end(JSON.stringify({ error: 'Not Found' }))
+    }
+  } catch (error) {
+    console.error(error)
+    res.statusCode = 500
+    res.end(JSON.stringify({ error: 'Internal Server Error' }))
+  }
+})
+
+const handleInboundPhoneCalls = async (req, res) => {
   const { to, from } = await parseBody(req)
 
   const testNumber = to in TEST_NUMBERS ? TEST_NUMBERS[to] : DEFAULT_TEST_NUMBER
@@ -56,7 +86,55 @@ const server = createServer(async (req, res) => {
   }
 
   res.end(JSON.stringify(response))
-})
+}
+
+const handleOutboundPhoneCalls = async (req, res) => {
+  const { parent_call_sid } = await parseBody(req)
+
+  const response = {
+    phone_calls: [
+      {
+        sid: crypto.randomUUID(),
+        parent_call_sid,
+        account_sid: crypto.randomUUID(),
+        carrier_sid: crypto.randomUUID(),
+        from: "1234",
+        call_direction: "outbound",
+        routing_parameters: {
+          address: null,
+          destination: "8551055100678",
+          dial_string_prefix: null,
+          plus_prefix: false,
+          national_dialing: false,
+          host: "27.109.112.141",
+          username: null,
+          sip_profile: "nat_gateway"
+        },
+        billing_parameters: {
+          enabled: false,
+          category: "outbound_calls",
+          billing_mode: "prepaid"
+        }
+      }
+    ]
+  }
+
+
+  res.end(JSON.stringify(response))
+}
+
+const handleRecordings = async (_req, res) => {
+  res.end(JSON.stringify({ sid: crypto.randomUUID(), url: 'https://api.somleng.org/cowbell.mp3'}))
+}
+
+const handleMediaStreams = async (_req, res) => {
+  res.end(JSON.stringify({ sid: crypto.randomUUID() }))
+}
+
+const parseBody = async (req) => {
+  const body = await new Response(req).text()
+  return body ? JSON.parse(body) : {}
+}
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`)
--- components/freeswitch/scripts/create_inbound_call.lua
+++ components/freeswitch/scripts/create_inbound_call.lua
@@ -3,8 +3,27 @@ freeswitch.consoleLog("info", "Creating inbound call")
 local json = require "cjson"
 local api = freeswitch.API()
 
-local response = api:execute("curl", "https://jsonplaceholder.typicode.com/todos/1")
-local data = json.decode(response)
+local call_platform_host = os.getenv("FS_CALL_PLATFORM_HOST")
 
+local payload = {
+  to = session:getVariable("sip_h_X-Somleng-Callee-Identity"),
+  from = session:getVariable("sip_h_X-Somleng-Caller-Identity"),
+  external_id = session:getVariable("uuid"),
+  host = session:getVariable("fs_host_ip"),
+  source_ip = session:getVariable("sip_h_X-Src-Ip"),
+  client_identifier = session:getVariable("sip_h_X-Somleng-Client-Identifier"),
+  variables = {
+    sip_from_host = session:getVariable("sip_from_host"),
+    sip_to_host = session:getVariable("sip_to_host"),
+    sip_network_ip = session:getVariable("sip_network_ip"),
+    sip_via_host = session:getVariable("sip_via_host")
+  }
+}
+
+local body = json.encode(payload)
+local params = call_platform_host .. "/services/inbound_phone_calls" .. " post " .. body
+
+local response = api:execute("curl", params)
+local data = json.decode(response)
 
 freeswitch.consoleLog("INFO", json.encode(data) .. "\n")
